9ca64f47d827c824fd73a9b7674edd91cbf30909
Tushar-K24 <april.tushar.edu@gmail.com>
2021-07-16 19:29:19 +0530

    repo-cleaning

--- recommender_system.py
+++ recommender_system.py
@@ -3,34 +3,43 @@
 
 # In[1]:
 
-%%writefile recommender_system.py
 
 import numpy as np
 import pandas as pd
+import pickle
+from sklearn.feature_extraction.text import TfidfVectorizer
 import streamlit as st
 
 
 # In[2]:
 
 
+filename = 'vectorizer.pkl'
+pickle_in = open(filename, 'rb')
+loaded_vectorizer = pickle.load(pickle_in)
+
+
+# In[3]:
+
+
 movies_df=pd.read_csv('tmdb_5000_movies.csv')
 credits_df=pd.read_csv('tmdb_5000_credits.csv')
 
 
-# In[3]:
+# In[4]:
 
 
 credits_df.rename(columns={'movie_id':'id'},inplace=True)
 merged_df=movies_df.merge(credits_df, on='id')
 
 
-# In[4]:
+# In[5]:
 
 
 merged_df[['overview','tagline']]=merged_df[['overview','tagline']].fillna('')
 
 
-# In[5]:
+# In[6]:
 
 
 features=['homepage','production_countries','release_date','runtime','title_y','original_title']
@@ -38,7 +47,7 @@ merged_df.drop(features, axis=1, inplace=True)
 merged_df=merged_df.rename(columns={'title_x':'title'})
 
 
-# In[6]:
+# In[7]:
 
 
 categories={
@@ -47,7 +56,7 @@ categories={
 merged_df=merged_df.replace(categories)
 
 
-# In[7]:
+# In[8]:
 
 
 r=merged_df['vote_average']
@@ -57,7 +66,7 @@ m=v.quantile(.90)
 merged_df['weighted_rating']=(r*v + c*m)/(v+m)
 
 
-# In[8]:
+# In[9]:
 
 
 std_popularity = merged_df['popularity'].std()
@@ -67,7 +76,7 @@ merged_df['rating_norm'] = merged_df['weighted_rating']/std_rating
 merged_df['score'] = (merged_df['popularity_norm'] + merged_df['rating_norm'])/2
 
 
-# In[9]:
+# In[10]:
 
 
 from ast import literal_eval
@@ -77,7 +86,7 @@ for feature in features:
     merged_df[feature]=merged_df[feature].apply(literal_eval)
 
 
-# In[10]:
+# In[11]:
 
 
 #extracting the director of the movie
@@ -88,7 +97,7 @@ def extract_director(crew):
     return np.nan #Nan if no director
 
 
-# In[11]:
+# In[12]:
 
 
 #extracting top 3 elements from each list
@@ -101,7 +110,28 @@ def get_top3(x):
     return []
 
 
-# In[12]:
+# In[13]:
+
+
+#extract cast
+def extract_cast(x):
+    if isinstance(x,list):
+        male=[i['name'] for i in x if i['gender']==2]
+        female=[i['name'] for i in x if i['gender']==1]
+        names=[]
+        if len(male)>3:
+            names.extend(male[:3])
+        else:
+            names.extend(male)
+        if len(female)>3:
+            names.extend(female[:3])
+        else:
+            names.extend(female)
+        return names
+    return []
+
+
+# In[14]:
 
 
 def remove_word_spaces(x):
@@ -113,46 +143,63 @@ def remove_word_spaces(x):
         return '' #no director
 
 
-# In[13]:
-
-
-merged_df['director']=merged_df['crew'].apply(extract_director)
-features=['genres','keywords','production_companies','cast']
-for feature in features:
-    merged_df[feature]=merged_df[feature].apply(get_top3)
-
-
-# In[14]:
-
-
-features=['genres','keywords','production_companies','cast','director']
-for feature in features:
-    merged_df[feature]=merged_df[feature].apply(remove_word_spaces)
-
-
 # In[15]:
 
 
-def create_string(x):
-    return (' '.join(x['genres']) + ' ' 
-            + ' '.join(x['keywords']) + ' ' 
-            + ' '.join(x['production_companies']) + ' '
-            + ' '.join(x['cast']) + ' '
-            + x['director']
-           )
-merged_df['word_string']=merged_df.apply(create_string,axis=1)
+#for overview and tagline
+def remove_capital_letters(x):
+    if isinstance(x,str):
+        return str.lower(x)
+    return ''
 
 
 # In[16]:
 
 
-from sklearn.feature_extraction.text import CountVectorizer
-
-count = CountVectorizer(stop_words='english')
-count_matrix = count.fit_transform(merged_df['word_string'])
+merged_df['director']=merged_df['crew'].apply(extract_director)
+features=['genres','keywords','production_companies']
+for feature in features:
+    merged_df[feature]=merged_df[feature].apply(get_top3)
+merged_df['cast']=merged_df['cast'].apply(extract_cast)
 
 
 # In[17]:
+
+
+features=['genres','keywords','production_companies','cast','director']
+for feature in features:
+    merged_df[feature] = merged_df[feature].apply(remove_word_spaces)
+
+
+# In[18]:
+
+
+features=['overview','tagline']
+for feature in features:
+    merged_df[feature] = merged_df[feature].apply(remove_capital_letters)
+
+
+# In[19]:
+
+
+def create_string(x):
+    return (' '.join(x['genres']) + ' ' 
+            + ' '.join(x['cast']) + ' '
+            + ' '.join(x['keywords']) + ' ' 
+            + x['director'] + ' '
+            + x['overview'] + ' ' 
+            #+ x['tagline']
+           )
+merged_df['word_string']=merged_df.apply(create_string,axis=1)
+
+
+# In[20]:
+
+#count = TfidfVectorizer(stop_words='english')
+count_matrix = loaded_vectorizer.fit_transform(merged_df['word_string'])
+
+
+# In[21]:
 
 
 from sklearn.metrics.pairwise import cosine_similarity
@@ -160,30 +207,51 @@ from sklearn.metrics.pairwise import cosine_similarity
 cosine_sim = cosine_similarity(count_matrix, count_matrix)
 
 
-# In[18]:
+# In[22]:
 
 
 merged_df = merged_df.reset_index()
 indices = pd.Series(merged_df.index, index=merged_df['title'])
 
 
-# In[19]:
+# In[23]:
 
 
 def get_recommendations(title, cosine_sim=cosine_sim):
-    if(title!="Select an Option"):
+    idx=indices[title]
+    similarity=list(enumerate(cosine_sim[idx]))
+    similarity.sort(key=lambda x:x[1],reverse=True)
+    similarity=similarity[1:11] # first movie will be the same
+    recommended_movies=[i[0] for i in similarity]
+    return recommended_movies
+
+
+# In[24]:
+
+def get_recommendations(title, cosine_sim=cosine_sim):
+    if title != 'Select an option':
         idx=indices[title]
         similarity=list(enumerate(cosine_sim[idx]))
         similarity.sort(key=lambda x:x[1],reverse=True)
         similarity=similarity[1:11] # first movie will be the same
         recommended_movies=[i[0] for i in similarity]
         for i in recommended_movies:
-            print(merged_df['title'].iloc[i])
+            st.write(merged_df['title'].iloc[i])
 
+# In[25]:
 
-# In[20]:
-
-
-option = st.selectbox('Select your favourite movie', merged_df['title'])
+option = st.selectbox('Select your favourite movie',merged_df['title'])
 get_recommendations(option)
+
+
+#filename = 'vectorizer.pkl'
+#pickle_out = open(filename, 'wb')
+#pickle.dump(count, pickle_out)
+#pickle_out.close()
+
+
+# In[ ]:
+
+
+
 
